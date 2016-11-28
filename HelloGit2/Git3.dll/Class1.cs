aa2cf61f7eeb509aa836f7c5b96b300a52e34362
Daniel Lindholm <dl@knowtronic.com>
2016-11-28 10:24:03 +0100

    Test 3

---
+++ HelloGit2/Git3.dll/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git3.dll
+{
+    public class Class2
+    {
+        public static void WriteHello()
+        {
+            Console.WriteLine("Hello GIT 2");
+
+        }
+    }
+}
